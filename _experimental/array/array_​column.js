0dbf392786166a67ff6abd466480b1a3bcf7b419
Biana Web Developer <it4craigslist@gmail.com>
2018-05-03 12:25:24 -0700

    partial _experimental

---
+++ _experimental/array/array_​column.js
@@ -0,0 +1,18 @@
+//binhgiik added
+
+function array_column(arr, type, prefix) {
+  // From: http://phpjs.org/functions
+  // +   original by: Brett Zamir (http://brett-zamir.me)
+  // %        note 1: Only works by extracting into global context (whether called in the global scope or
+  // %        note 1: within a function); also, the EXTR_REFS flag I believe can't be made to work
+  // *     example 1: size = 'large';
+  // *     example 1: var_array = {'color' : 'blue', 'size' : 'medium', 'shape' : 'sphere'};
+  // *     example 1: extract(var_array, 'EXTR_PREFIX_SAME', 'wddx');
+  // *     example 1: color+'-'+size+'-'+shape+'-'+wddx_size;
+  // *     returns 1: 'blue-large-sphere-medium'
+  //array array_column ( array $input , mixed $column_key [, mixed $index_key = NULL ] )
+  
+  var arr = [];
+  
+  return arr;
+}
